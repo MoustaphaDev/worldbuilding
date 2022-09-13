e8750bd228189e7efadcd82f503a0f861b0b04b5
Aleksandr Vasilenko <phenomen@proton.me>
2022-09-13 13:26:54 -0500

    Header links and Footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,22 @@
 import { defineConfig } from "astro/config";
-import tailwind from "@astrojs/tailwind";
 
+import tailwind from "@astrojs/tailwind";
 import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()],
+  integrations: [tailwind(), mdx(), sitemap()],
   markdown: {
+    rehypePlugins: [
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "append",
+        },
+      ],
+    ],
     remarkPlugins: [setFallbackLayout],
     extendPlugins: true,
   },
